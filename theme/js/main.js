da3d6b96ab2351e8757637a87a2412ab0b9c4e09
welltrojan <wellingtontrojan@gmail.com>
2021-02-12 18:15:34 -0300

    Add CDN JQuery Mask

--- theme/js/main.js
+++ theme/js/main.js
@@ -12,6 +12,7 @@ function include(file) {
 // Include CDNs Files
 include('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js');
 include('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js');
+include('https://igorescobar.github.io/jQuery-Mask-Plugin/js/jquery.mask.min.js');
 
 // Include Dependecies Files
 include(`${site_url}/js/bundle/owl.carousel.min.js`);
