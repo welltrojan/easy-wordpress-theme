32ef5aa14432880d68c93b064fa919dc88dd0b9e
welltrojan <wellingtontrojan@gmail.com>
2021-02-12 21:58:39 -0300

    Fix Theme support for Thumbnails

--- theme/includes/admin/Admin.php
+++ theme/includes/admin/Admin.php
@@ -7,7 +7,7 @@ class Admin
 
 		// Theme Support
 		// Specific theme support thumbnail for pages and posts
-		add_theme_support( 'post-thumbnails', array( 'post', 'page' ));
+		add_theme_support( 'post-thumbnails' );
 		// Theme support for Woocommerce
 		add_theme_support( 'woocommerce' );
 		add_theme_support( 'wc-product-gallery-slider' );
